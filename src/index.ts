8e6d5c8c4ed5c4f1a56c4b0d57870465c44d10d4
DCarla <dcsouza119@gmail.com>
2023-07-03 16:46:48 -0300

    apis-e-express-exercicios-feitos

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { product } from "./database";
 import { getAllUsers } from "./database";
 import { getAllProducts } from "./database";
 import { procurarProdutoPorNome } from "./database";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { TProducts, TUsers } from "./types";
 
 console.log("funcionou");
 // console.log(users);
@@ -16,8 +19,74 @@ createProduct(
   "https://cdn.pixabay.com/photo/2020/04/04/15/04/click-5002627_1280.jpg"
 );
 
-console.log(users);
+// console.log(users);
 
-console.table(getAllUsers());
-console.table(getAllProducts());
-console.table(procurarProdutoPorNome("mouse"));
+// console.table(getAllUsers());
+// console.table(getAllProducts());
+// console.table(procurarProdutoPorNome("mouse"));
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {});
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.status(200).send("Pong!");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  res.status(200).send(users);
+});
+
+app.get("/product", (req: Request, res: Response) => {
+  res.status(200).send(product);
+});
+
+app.get("/product/search", (req: Request, res: Response) => {
+  const name = req.query.name as string;
+  const resultado = product.filter((product) =>
+    product.name.toLowerCase().includes(name.toLowerCase())
+  );
+  res.status(200).send(resultado);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  const id = req.body.id as string;
+  const name = req.body.name as string;
+  const email = req.body.email as string;
+  const password = req.body.password as string;
+
+  const newUser: TUsers = {
+    id,
+    name,
+    email,
+    password,
+    createdAt: new Date().toISOString(),
+  };
+
+  users.push(newUser);
+
+  res.status(201).send("Cadastro realizado com sucesso");
+});
+
+app.post("/product", (req: Request, res: Response) => {
+  const id = req.body.id as string;
+  const name = req.body.name as string;
+  let price = req.body.price as number;
+  const description = req.body.description as string;
+  const imageUrl = req.body.description as string;
+
+  const newProduct: TProducts = {
+    id,
+    name,
+    price,
+    description,
+    imageUrl,
+  };
+
+  product.push(newProduct);
+
+  res.status(201).send("Produto cadastrado com sucesso");
+});
